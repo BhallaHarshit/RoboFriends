1bd089f1c97fbe7f23230279e329911ab66769f4
Harshit <harshitbhalla12@gmail.com>
2020-06-21 00:15:51 +0530

    cleaning up and showing all robots

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 const Card = ({ name, id, email}) => {
     return(
+        <Fragment>
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src ={`https://robohash.org/${id}?size=200x200`}/>
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
+                <img alt='robots' src ={`https://robohash.org/${id}?size=200x200`}/>
+                <div>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div>
             </div>
-        </div>
+        </Fragment> 
     );
 }
 
---
+++ src/CardList.js
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react'
+import Card from './Card'
+
+const CardList = ({ robots }) => {
+    return(
+        <Fragment>
+            {
+                robots.map((robot, i) => {
+                    return (
+                        <Card
+                            key={ i }
+                            id={ robot.id }
+                            name={ robot.name } 
+                            email={ robot.email }
+                        />
+                    )
+                })
+            }
+        </Fragment>
+    )
+}
+
+export default CardList;
